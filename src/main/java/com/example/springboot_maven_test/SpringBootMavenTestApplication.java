0c92831f856d563dd1679c5272d080ce3d30d361
MadLogarithm <1141519626@qq.com>
2024-01-11 14:55:34 +0800

    chore(): init code framework.

---
+++ src/main/java/com/example/springboot_maven_test/SpringBootMavenTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_maven_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMavenTestApplication.class, args);
+    }
+
+}
