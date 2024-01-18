15d6d2fd8f81732bd7de6efd4d4d28acdb971efe
MadLogarithm <1141519626@qq.com>
2024-01-18 15:44:17 +0800

    chore(): add MapperScan.

--- src/main/java/com/example/springboot_maven_test/SpringBootMavenTestApplication.java
+++ src/main/java/com/example/springboot_maven_test/SpringBootMavenTestApplication.java
@@ -1,9 +1,11 @@
 package com.example.springboot_maven_test;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.springboot_maven_test.mapper")
 public class SpringBootMavenTestApplication {
 
     public static void main(String[] args) {
