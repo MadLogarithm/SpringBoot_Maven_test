e47e95bb124ba8f49c397905b567c4e6c4da50c8
MadLogarithm <1141519626@qq.com>
2024-01-18 15:42:25 +0800

    feat(): add UserMapper interface.

---
+++ src/main/java/com/example/springboot_maven_test/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.example.springboot_maven_test.mapper;
+
+import com.example.springboot_maven_test.model.User;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    List<User> selectAllUser();
+    User selectUserById(int id);
+}
